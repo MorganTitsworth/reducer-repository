da12ae8a9c540128e3fe78d2f0c43095252cb1af
Duane Mitchell <d.mitchell@erasolutions.us>
2024-03-22 12:42:38 -0400

    defualt layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { UserOne } from './components/UserOne';
-import { UserTwo } from './components/UserTwo';
-import { ToDoProvider } from './hooks/ToDoContext';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <ToDoProvider>
-          <UserOne/>
-          <UserTwo/>
-      </ToDoProvider>
+
       </header>
     </div>
   );
